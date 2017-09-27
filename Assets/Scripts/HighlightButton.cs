a81ba7e8e763b3f0e9a4f41a1d4b191ef7578b26
KT <kyungtyu@hotmail.com>
2017-09-27 01:12:55 -0700

    Added submenu for extras similar to play, now submenu buttons highlight properly at the beginning.

---
+++ Assets/Scripts/HighlightButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighlightButton : MonoBehaviour {
+
+	// Use this for initialization
+	public void ChangeHighlight(Button newButton)
+    {
+        if(newButton != null)
+        {
+            newButton.Select();
+        }
+    }
+}
